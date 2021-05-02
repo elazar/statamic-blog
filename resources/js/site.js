243cdbe8e25cf56239595d028518a70982fc7cfc
Matt Turland <matt.turland@stacksports.com>
2021-05-02 09:27:54 -0500

    Add Matomo tracking code

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,5 +1,18 @@
 // Alpine.js handles the show/hide of the mobile nav
 import 'alpinejs'
 
+// Matomo
+var _paq = window._paq = window._paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://matomo.matthewturland.com/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '1']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
+
+// Prism.js
 import Prism from 'prismjs'
 Prism.highlightAll()
