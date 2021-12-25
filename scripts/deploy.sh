5b397d420b4e270ccdf3d6c845ab2e5481de0772
Matthew Turland <tobias382@gmail.com>
2021-12-25 07:15:34 -0600

    Improve deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-#rsync -rvz ./storage/app/static/ pi:~/statamic-blog/
+
+BUILD_REMOTE="origin"
+BUILD_BRANCH="main"
+
 cd ./storage/app/static
+sudo chown -R pi:pi .
+[[ -d .git ]] || git init -b $BUILD_BRANCH
+git config pull.rebase false
+git remote | grep $BUILD_REMOTE >/dev/null
+[[ $? -eq 0 ]] || git remote add $BUILD_REMOTE git@github.com:elazar/elazar.github.io.git
+git fetch $BUILD_REMOTE $BUILD_BRANCH
+git checkout -f $BUILD_BRANCH
 git add *
 git commit -m 'Build'
-git push origin main
+git push $BUILD_REMOTE $BUILD_BRANCH
