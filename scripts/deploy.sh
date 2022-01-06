b3e66cfdd2d16e4c6f381dfb9081a9f351a5a006
Matthew Turland <tobias382@gmail.com>
2022-01-06 13:40:29 -0600

    Actually fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,8 +10,7 @@ cd $BUILD_PATH
 git config pull.rebase false
 git remote | grep $BUILD_REMOTE >/dev/null
 [[ $? -eq 0 ]] || git remote add $BUILD_REMOTE git@github.com:elazar/elazar.github.io.git
-git fetch $BUILD_REMOTE $BUILD_BRANCH
-git checkout -f $BUILD_BRANCH
+echo 'matthewturland.com' > CNAME
 git add *
 git commit -m 'Build'
-git push $BUILD_REMOTE $BUILD_BRANCH
+git push -f $BUILD_REMOTE $BUILD_BRANCH
